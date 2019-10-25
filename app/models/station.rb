7644d72b2a72f85cf32ae2f3caa9686c9d0fa115
Leiya Kenney <leiya.kenney@gmail.com>
2019-10-25 11:20:44 -0600

    Initializes station's properties correctly

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,10 +1,11 @@
 class Station
-  attr_reader :station_name, :address, :fuel_type, :distance
+  attr_reader :name, :address, :fuel_type, :distance
 
   def initialize(station)
-    @name = station[:fuel_stations][:station_name]
-    @address = station[:fuel_stations][:address]
-    @fuel_type = station[:fuel_stations][:fuel_type]
-    @distance = station[:fuel_stations][:distance]
+    # binding.pry
+    @name = station[:name]
+    @address = station[:address]
+    @fuel_type = station[:fuel_type]
+    @distance = station[:distance]
   end
 end
