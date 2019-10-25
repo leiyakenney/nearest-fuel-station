b8dfaa9adbcb4a3bac1f220bce4f3680af8d1739
Leiya Kenney <leiya.kenney@gmail.com>
2019-10-25 09:31:01 -0600

    Creates Station Search Results facade

---
+++ app/facades/station_search_results.rb
@@ -0,0 +1,20 @@
+class StationSearchResults
+  def initialize
+    @name = name
+    @address = address
+    @fuel_type = fuel_type
+    @distance = distance
+    @access_time = access_time
+  end
+
+  def stations
+    conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
+      faraday.headers["X-API-KEY"] = ENV['PROPUBLICA_API_KEY']
+      faraday.adapter Faraday.default_adapter
+    end
+
+    response = conn.get("/congress/v1/members/house/#{state}/current.json")
+
+    JSON.parse(response.body, symbolize_names: true)[:results]
+  end
+end
