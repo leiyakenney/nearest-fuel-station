3187eeec79937c291371816d0a38c1dcb3b7898f
Leiya Kenney <leiya.kenney@gmail.com>
2019-10-25 10:57:23 -0600

    Changes facade name to closest_station_facade

---
+++ app/facades/closest_station_facade.rb
@@ -0,0 +1,29 @@
+class ClosestStationFacade
+  def initialize(address)
+    @address = address
+  end
+
+  def station
+    conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/") do |faraday|
+      faraday.headers["X-API-KEY"] = ENV['NREL_API_KEY']
+      faraday.adapter Faraday.default_adapter
+    end
+
+    response = conn.get("v1/nearest.json?api_key=#{ENV['NREL_API_KEY']}&location=1771+17th+St+Denver+CO&limit=1")
+    top_station_data = JSON.parse(response.body, symbolize_names: true)
+
+    top_station_data.map do |data|
+      Station.new(data)
+    end 
+  end
+  #facade.station[:fuel_stations][:station_name] = name
+  #facade.station[:fuel_stations][:street_address] = address
+  #facade.station[:fuel_stations][:city] = city
+  #facade.station[:fuel_stations][:state] = state
+  #facade.station[:fuel_stations][:zip] = zip
+  #facade.station[:fuel_stations][:distance] = distance
+
+
+  private
+  attr_reader :address
+end
