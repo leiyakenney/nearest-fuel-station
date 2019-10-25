e5e3b125bc426be162cbe177499a17b60d3e7dee
Leiya Kenney <leiya.kenney@gmail.com>
2019-10-25 11:08:02 -0600

    Recreates station model

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,1 +1,10 @@
+class Station
+  attr_reader :station_name, :address, :fuel_type, :distance
 
+  def initialize(station)
+    @name = station[:fuel_stations][:station_name]
+    @address = station[:fuel_stations][:address]
+    @fuel_type = station[:fuel_stations][:fuel_type]
+    @distance = station[:fuel_stations][:distance]
+  end
+end
